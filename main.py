91fd85fbd0cd1a5bd1c960ec8b9a32762e9a4031
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-09-18 02:27:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,8 +71,9 @@ if choice == 'Diabetes Prediction':
 
 
 
+
 # Heart Disease Prediction Page
-if choice == 'Heart Disease Prediction':
+if (selected == 'Heart Disease Prediction'):
     
     # page title
     st.title('Heart Disease Prediction using ML')
@@ -80,40 +81,40 @@ if choice == 'Heart Disease Prediction':
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        age = st.text_input('Age ( 29 - 77)')
+        age = st.text_input('Age')
         
     with col2:
-        sex = st.text_input('Sex (male = 1 and female = 0)')
+        sex = st.text_input('Sex')
         
     with col3:
-        cp = st.text_input('Chest Pain types ( 0 - 3) ')
+        cp = st.text_input('Chest Pain types')
         
     with col1:
-        trestbps = st.text_input('Resting Blood Pressure(94 - 200)')
+        trestbps = st.text_input('Resting Blood Pressure')
         
     with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl ( 126 - 564) ')
+        chol = st.text_input('Serum Cholestoral in mg/dl')
         
     with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl ( 0 -1)')
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
         
     with col1:
-        restecg = st.text_input('Resting Electrocardiographic results ( 0-2)')
+        restecg = st.text_input('Resting Electrocardiographic results')
         
     with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved ( 71 -202)')
+        thalach = st.text_input('Maximum Heart Rate achieved')
         
     with col3:
-        exang = st.text_input('Exercise Induced Angina ( 0 -1 )')
+        exang = st.text_input('Exercise Induced Angina')
         
     with col1:
-        oldpeak = st.text_input('ST depression induced by exercise ( 0 -6.2)')
+        oldpeak = st.text_input('ST depression induced by exercise')
         
     with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment( 0 -2)')
+        slope = st.text_input('Slope of the peak exercise ST segment')
         
     with col3:
-        ca = st.text_input('Major vessels colored by flourosopy (0-4)')
+        ca = st.text_input('Major vessels colored by flourosopy')
         
     with col1:
         thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
@@ -135,9 +136,7 @@ if choice == 'Heart Disease Prediction':
           heart_diagnosis = 'The person does not have any heart disease'
         
     st.success(heart_diagnosis)
-        
-    
-    
+
 
 # Parkinson's Prediction Page
 if choice == "Parkinsons Prediction":
